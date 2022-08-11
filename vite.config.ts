67ab04fd16e0c40effa19aaccc852bc67f9669ed
Edwin Bartunek <edwin@devhart.io>
2022-08-11 11:27:01 +0900

    correcting import for ga

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteRadar from 'vite-plugin-radar'
+import {VitePluginRadar} from 'vite-plugin-radar'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    ViteRadar({
+    VitePluginRadar({
       enableDev: false,
-    analytics: {
-      id: 'G-MCCJK1H53R',
-    },
-  })],
- 
+      analytics: {
+        id: 'G-MCCJK1H53R',
+      },
+    }),
+  react(),
+   
+], 
 })
